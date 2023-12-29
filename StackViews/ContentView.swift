1218df1d4aed7c5fa07be3cb56112ace8385dd4b
Edwin <edwin12sv@gmail.com>
2023-12-28 23:35:44 -0600

    Initial Commit

---
+++ StackViews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StackViews
+//
+//  Created by elsalvador on 28/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StackViews/StackViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewsApp.swift
+//  StackViews
+//
+//  Created by elsalvador on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StackViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
